c817e68904b6d775952ef51d44426b2236c51a17
mardiniii <sezama03@hotmail.com>
2017-07-20 22:56:03 +0200

    Adds the create method for the orders controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,15 @@
 class Order < ApplicationRecord
-  belongs_to :user
+  before_create :set_total!
 
   validates :total, presence: true,
                     numericality: { greater_than_or_equal_to: 0 }
-
   validates :user_id, presence: true
 
+  belongs_to :user
   has_many :placements
   has_many :products, through: :placements
+
+  def set_total!
+    self.total = products.map(&:price).sum
+  end
 end
--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -32,4 +32,41 @@ RSpec.describe Api::V1::OrdersController, type: :controller do
 
     it { should respond_with 200 }
   end
+
+  describe 'POST #create' do
+    before(:each) do
+      current_user = FactoryGirl.create :user
+      api_authorization_header current_user.auth_token
+
+      product_one = FactoryGirl.create :product
+      product_two = FactoryGirl.create :product
+      order_params = {
+        product_ids: [product_one.id, product_two.id]
+      }
+      post :create, params: {
+        user_id: current_user.id,
+        order: order_params
+      }
+    end
+
+    it 'returns the just user order record' do
+      order_response = json_response
+      expect(order_response[:id]).to be_present
+    end
+
+    it { should respond_with 201 }
+  end
+
+  describe '#set_total!' do
+    before(:each) do
+      product_one = FactoryGirl.create :product, price: 100
+      product_two = FactoryGirl.create :product, price: 85
+
+      @order = FactoryGirl.build :order, product_ids: [product_one.id, product_two.id]
+    end
+
+    it 'returns the total amount to pay for the products' do
+      expect{ @order.set_total! }.to change{ @order.total }.from(0).to(185)
+    end
+  end
 end
